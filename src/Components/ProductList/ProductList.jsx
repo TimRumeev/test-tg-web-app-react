31903235f783c24034dc1d41b2b414bb686471b5
TimRumeev <trumeev@mail.com>
2024-06-18 22:26:41 +0500

    asdasdas

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
 			totalPrice: getTotalPrice(addedItems),
 			queryId,
 		}
-		fetch("http://192.168.0.56:8000/web-data", {
+		fetch("http://185.184.55.37:8000/web-data", {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
