d77f67cc14393b75c0a068d7876586d582aeb908
TimRumeev <trumeev@mail.com>
2024-06-18 17:50:35 +0500

    asdsadasd

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -15,7 +15,7 @@ const Form = () => {
 			subject,
 		}
 		tg.sendData(JSON.stringify(data))
-	}, [tg])
+	}, [tg, country, city, subject])
 
 	useEffect(() => {
 		tg.onEvent("mainButtonClicked", onSendData)
